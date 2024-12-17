96f26f622c63b330cff27a5acaf6bc8626fbb57a
Mohamed Sherif <Mohamed Sherif@DESKTOP-AR3TO2V>
2024-12-17 13:56:54 +0300

    Add project files.

---
+++ C# Basic2/Program.cs
@@ -0,0 +1,149 @@
+﻿namespace C__Basic2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1- Write a program that takes a number from the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+            //Console.WriteLine("Enter Number to check");
+            //int.TryParse(Console.ReadLine(), out int number);
+
+
+            //if (number % 3 == 0 && number % 4 == 0)
+            //{
+            //    Console.WriteLine("Yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("no");
+
+            //}
+
+
+
+            #endregion
+
+            #region 2- Write a program that allows the user to insert an integer then print negative if it is negative number otherwise print positive.
+            //Console.WriteLine("Enter Number to check Negative or Positive");
+            //int.TryParse(Console.ReadLine(), out int number1);
+
+            //if (number1 > 0)
+            //    Console.WriteLine("Positive");
+            //else if (number1 < 0)
+            //    Console.WriteLine("Negative");
+            //else Console.WriteLine("Number = 0 ");
+
+
+            #endregion
+
+            #region 3- Write a program that takes 3 integers from the user then prints the max element and the min element.
+            //Console.WriteLine("Enter Number 1 ");
+            //double.TryParse(Console.ReadLine(), out double x);
+            //Console.WriteLine("Enter Number 2");
+            //double.TryParse(Console.ReadLine(), out double y);
+            //Console.WriteLine("Enter Number 3");
+            //double.TryParse(Console.ReadLine(), out double z);
+            //Console.WriteLine($"Max Element= {Math.Max(x, Math.Max(y, z))} Min Element = {Math.Min(x, Math.Min(y, z))}");
+            #endregion
+
+            #region 4-Write a program that allows the user to insert an integer number then check If a number is even or odd.
+            //Console.WriteLine("Enter Number to check even or odd");
+            //int.TryParse(Console.ReadLine(), out int number2);
+
+            //if (number2 % 2 == 0)
+            //    Console.WriteLine("Even");
+            //else Console.WriteLine("odd");
+
+
+            #endregion
+
+            #region 5- Write a program that takes character from the user then if it is a vowel chars (a,e,I,o,u) then print (vowel) otherwise print (consonant).
+            //Console.WriteLine("Enter char to check vowel or constant");
+            //string ch =Console.ReadLine();
+
+            //if (ch == "a" | ch == "e" | ch == "i" | ch == "o" | ch == "u" | ch == "A" | ch == "E" | ch == "I" | ch == "O" | ch == "U")
+            //    Console.WriteLine("Vowel");
+            //else Console.WriteLine("Constant");
+
+
+
+
+            #endregion
+
+            #region 7-Write a program that allows the user to insert an integer then print a multiplication table up to 12.
+            //Console.WriteLine("Enter Number");
+            //int.TryParse(Console.ReadLine(), out int number);
+
+            //Console.WriteLine($"{number} {number*2} {number * 3} {number * 4} {number * 5} {number * 6} {number * 7} {number * 8} {number * 9} {number * 10} {number * 11} {number * 12}");
+
+
+            #endregion
+
+            #region 9- Write a program that takes two integers then prints the power.
+            //Console.WriteLine("Enter Number ");
+            //double.TryParse(Console.ReadLine(), out double x);
+            //Console.WriteLine("Enter Power");
+            //double.TryParse(Console.ReadLine(), out double y);
+            //double z = Math.Pow(x, y);
+
+            //Console.WriteLine($"{x} power {y} = {z}");
+
+
+            #endregion
+
+            #region 10- Write a program to enter marks of five subjects and calculate total, average and percentage.
+            //Console.WriteLine("enter marks of five subjects");
+            //Console.WriteLine("Subject 1");
+            //double.TryParse(Console.ReadLine(), out double m1);
+            //Console.WriteLine("Subject 2");
+            //double.TryParse(Console.ReadLine(), out double m2);
+            //Console.WriteLine("Subject 3");
+            //double.TryParse(Console.ReadLine(), out double m3);
+            //Console.WriteLine("Subject 4");
+            //double.TryParse(Console.ReadLine(), out double m4);
+            //Console.WriteLine("Subject 5");
+            //double.TryParse(Console.ReadLine(), out double m5);
+
+            //int p = (int)((m1 + m2 + m3 + m4 + m5) / 5);
+
+            //Console.WriteLine($"Total Marks = {m1+m2+m3+m4+m5}");
+            //Console.WriteLine($"Average Marks ={p}");
+            //Console.WriteLine($"Percentegr Marks ={p}%");
+
+
+            #endregion
+
+            #region 11- Write a program to input the month number and print the number of days in that month.
+            //Console.Write("Please Enter A Month Number to print number of days in it");
+            //int.TryParse(Console.ReadLine(), out int monthNumber);
+            //switch (monthNumber)
+            //{
+            //    case 2:
+            //        Console.WriteLine("28 days");
+            //        break;
+            //    case 4|6|9|11:
+            //        Console.WriteLine("30 days");
+            //        break;
+            //    default:
+            //        Console.WriteLine("31 days");
+            //        break;
+            //}
+            #endregion
+
+            #region 13- Write a program to allow the user to enter a string and print the REVERSE of it.
+            //Console.Write("Enter a String : ");
+            //string originalString = Console.ReadLine();
+            //string reverseString = string.Empty;
+            //foreach (char c in originalString)
+            //{
+            //    reverseString = c + reverseString;
+            //}
+            //Console.Write($"Reverse String is : {reverseString} ");
+
+            #endregion
+
+            #region 16-Write a program in C# Sharp to convert a decimal number into binary without using an array.
+            #endregion
+        }
+    }
+}
